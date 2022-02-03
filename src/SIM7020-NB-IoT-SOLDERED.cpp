3e0e433f27b30fa5399df70d6733d4c08444a799
Main Push Robot <robot@example.com>
2022-02-03 13:46:51 +0000

    Committing dev changes

---
+++ src/SIM7020-NB-IoT-SOLDERED.cpp
@@ -0,0 +1,10 @@
+/**
+ **************************************************
+ *
+ * @file        SIM7020-NB-IoT-SOLDERED.cpp
+ * @brief       Intentionally blank.
+ *
+ *
+ * @copyright   GNU General Public License v3.0
+ * @authors     Goran Juric @ soldered.com
+ ***************************************************/
---
+++ src/SIM7020-NB-IoT-SOLDERED.h
@@ -0,0 +1,17 @@
+/**
+ **************************************************
+ *
+ * @file        SIM7020-NB-IoT-SOLDERED.h
+ * @brief       Header file for sensor specific code.
+ *
+ *
+ * @copyright   GNU General Public License v3.0
+ * @authors     Goran Juric @ soldered.com
+ ***************************************************/
+
+#ifndef __SIM7020E__
+#define __SIM7020E__
+
+#include "libs/Magellan_SIM7020E/src/Magellan_SIM7020E.h"
+
+#endif
