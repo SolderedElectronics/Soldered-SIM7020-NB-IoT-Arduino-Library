69b22170d8a0064f8d8d1d834268a5c872795510
Main Push Robot <robot@example.com>
2022-02-21 15:30:07 +0000

    Committing dev changes

--- src/AltSoftSerial.cpp
+++ src/AltSoftSerial.cpp
@@ -29,7 +29,6 @@
 // Version 1.1: Improve performance in receiver code
 //
 // Version 1.0: Initial Release
-#ifdef Alt
 #include "AltSoftSerial.h"
 #include "config/AltSoftSerial_Boards.h"
 #include "config/AltSoftSerial_Timers.h"
@@ -397,6 +396,4 @@ void ftm0_isr(void)
     if (flags & (1 << 6) && (FTM0_C6SC & 0x40))
         altss_compare_a_interrupt();
 }
-#endif
-
 #endif
