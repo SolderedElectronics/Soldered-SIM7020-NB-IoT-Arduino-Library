720c2c19faaf1946b3322b7bdee6cba9ceb8b015
Docs Robot <robot@example.com>
2022-02-22 12:18:05 +0000

    Committing documentation changes

--- docs/board_8h.js
+++ docs/board_8h.js
@@ -3,5 +3,5 @@ var board_8h =
     [ "serialPort", "board_8h.html#aa22311f7e3240d97c1df2424e9d9c542", null ],
     [ "serialPort", "board_8h.html#ac5dd765e5e61c933fd1aff77eedcde0e", null ],
     [ "serialPort", "board_8h.html#aa055e9c60feb38ad29373622eda46aa4", null ],
-    [ "serialPort", "board_8h.html#af2de4b32d939c7b7faf877645f89ca02", null ]
+    [ "serialPort", "board_8h.html#a82cfdcc6ceea2d3ca869c56fd5301bc4", null ]
 ];
