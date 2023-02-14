ec48657fd77e6caf751ba66ad123f60acece18b6
Docs Robot <robot@example.com>
2023-02-14 16:07:15 +0000

    Committing documentation changes

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -25,9 +25,7 @@
 var NAVTREE =
 [
   [ "SIM7020 NB-IoT Library", "index.html", [
-    [ "NB-IoT communications SIM7020 breakout Arduino library", "index.html", [
-      [ "Have fun!", "index.html#autotoc_md8", null ]
-    ] ],
+    [ "NB-IoT communications SIM7020 breakout Arduino library", "index.html", "index" ],
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
